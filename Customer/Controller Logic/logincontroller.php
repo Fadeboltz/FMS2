51986bb512c276192d052095b3b826906c38c733
Nabil Nasim <nabilmohammednasim@gmail.com>
2026-01-08 21:38:16 +0600

    add test_input function added

--- Customer/Controller Logic/logincontroller.php
+++ Customer/Controller Logic/logincontroller.php
@@ -3,3 +3,10 @@
 $email = $password = "";
 $emailErr = $passErr = "";
 $loginSuccess = false;
+
+function test_input($data) {
+    $data = trim($data);
+    $data = stripslashes($data); 
+    $data = htmlspecialchars($data); 
+    return $data;
+}
