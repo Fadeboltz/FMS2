7cd1f498226b89de14c62d9966c474aac331d178
Nabil Nasim <nabilmohammednasim@gmail.com>
2026-01-09 22:49:53 +0600

    form details added for registration

--- Customer/View/Registration.php
+++ Customer/View/Registration.php
@@ -10,6 +10,34 @@
 <body>
     <h1><center>Register Form</center></h1>
     <form action="../Controller Logic/registrationcontroller.php" method="post">
+
+        <label for="name">Username:</label><br>
+        <input type="text" id="name" name="name" required><br><br>
+
+        <label for="contactno">Contact No:</label><br>
+        <input type="text" id="contactno" name="contactno" required><br><br>
+
+        <label for="address">Address:</label><br>
+        <input type="text" id="address" name="address" required><br><br>
+       
+
+        <label for="email">Email:</label><br>
+        <input type="email" id="email" name="email" required><br><br>
+
+        <label for="password">Password:</label><br>
+        <input type="password" id="password" name="password" required><br><br>
+
+        <label for="confirmpassword">Confirm Password:</label><br>
+        <input type="password" id="confirmpassword" name="confirmpassword" required><br><br>
+
+        <label for="accounttype">Account Type:</label><br>
+        <select id="accounttype" name="accounttype" required>
+            <option value="customer">Customer</option>
+            <option value="kitchenstaff">Kitchen Staff</option>
+        </select><br><br>
+    
+        <button type="submit">Register</button>
+
     </form>
 
 </body>
