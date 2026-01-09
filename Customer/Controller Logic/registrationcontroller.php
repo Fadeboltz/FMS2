8f26000256bed3f19949cf7ed74f6b6d1204b39f
Nabil Nasim <nabilmohammednasim@gmail.com>
2026-01-09 23:00:30 +0600

    add form field handling in registration controller

--- Customer/Controller Logic/registrationcontroller.php
+++ Customer/Controller Logic/registrationcontroller.php
@@ -3,6 +3,13 @@ include '../Model/user.php';
 
 if($_SERVER['REQUEST_METHOD'] == 'POST') {
 
+    $name = $_POST['name'];
+    $contactno = $_POST['contactno'];
+    $address = $_POST['address'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $confirmPassword = $_POST['confirmpassword']; 
+    $accountType = $_POST['accounttype'];
 
 }
 ?>
