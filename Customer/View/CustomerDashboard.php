5aad34ae37588bb9389e9cde8dafb1e69652baba
Nabil Nasim <nabilmohammednasim@gmail.com>
2026-01-09 23:11:04 +0600

    dashboard demo

---
+++ Customer/View/CustomerDashboard.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Customer Dashboard</title>
+    <link rel="stylesheet" href="../Stylesheet/CustomerDashboard.css">
+</head>
+<body>
+    <h1>Welcome to the Customer Dashboard</h1>
+    <p>This is where customers can view their information and manage their account.</p>
+</body>
+</html>
