cb5e367234b1b4518d234c25c7d211bdf0351778
Nabil Nasim <nabilmohammednasim@gmail.com>
2026-01-09 21:34:02 +0600

    remove exit statement

--- Customer/Controller Logic/logincontroller.php
+++ Customer/Controller Logic/logincontroller.php
@@ -39,8 +39,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         {
             echo"<script>alert('Login Successful');</script>";
             $loginSuccess = true;
-
-            exit();
         } 
         else 
         {
