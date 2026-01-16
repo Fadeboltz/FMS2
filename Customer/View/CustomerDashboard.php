2d86adcf6b9e042f118232aa76bdb8c12075c1cb
Nabil Nasim <nabilmohammednasim@gmail.com>
2026-01-16 18:36:17 +0600

    Add filter buttons to CustomerDashboard view

--- Customer/View/CustomerDashboard.php
+++ Customer/View/CustomerDashboard.php
@@ -17,4 +17,17 @@
         <a href="Login.php">Logout</a>
     </div>
 </div>
+
+<div class="filters">
+    <a href="CustomerDashboard.php?category=All" class="filter-btn">All</a>
+    <a href="CustomerDashboard.php?category=Burger" class="filter-btn">🍔 Burger</a>
+    <a href="CustomerDashboard.php?category=Pizza" class="filter-btn">🍕 Pizza</a>
+    <a href="CustomerDashboard.php?category=Pancake" class="filter-btn">🥞 Pancake</a>
+    <a href="CustomerDashboard.php?category=Shawarma" class="filter-btn">🌯 Shawarma</a>
+    <a href="CustomerDashboard.php?category=Taco" class="filter-btn">🌮 Taco</a>
+    <a href="CustomerDashboard.php?category=Poutine" class="filter-btn">🍛 Poutine</a>
+    <a href="CustomerDashboard.php?category=Ramen" class="filter-btn">🍜 Ramen</a>
+    <a href="CustomerDashboard.php?category=Sushi" class="filter-btn">🍣 Sushi</a>
+    <a href="CustomerDashboard.php?category=Cake" class="filter-btn">🍰 Cake</a>
+</div>
 </body>
