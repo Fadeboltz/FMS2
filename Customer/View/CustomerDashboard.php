3867f78a9be9250e75d0a28d95e0657ff47d3f9e
Nabil Nasim <nabilmohammednasim@gmail.com>
2026-01-16 18:08:43 +0600

    Add CustomerDashboard view with controller inclusion and basic structure

---
+++ Customer/View/CustomerDashboard.php
@@ -0,0 +1,7 @@
+<?php include '../Controller/CustomerDashboardController.php'; ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Raphael's World</title>
+    <link rel="stylesheet" href="../Stylesheet/CustomerDashboard.css">
+</head>
