a3c320a0aa5b1d5aa9b9122a74a5611c452d9d0a
Nabil Nasim <nabilmohammednasim@gmail.com>
2026-01-16 18:09:59 +0600

    Add navbar and user greeting to CustomerDashboard view

--- Customer/View/CustomerDashboard.php
+++ Customer/View/CustomerDashboard.php
@@ -5,3 +5,16 @@
     <title>Raphael's World</title>
     <link rel="stylesheet" href="../Stylesheet/CustomerDashboard.css">
 </head>
+
+<body>
+
+<div class="navbar">
+    <h2>Food Panda Clone</h2>
+    <div class="nav-links">
+        <span>Hi, <?php echo htmlspecialchars($user_email); ?></span>
+        <a href="#">My Cart (0)</a>
+        <a href="#">My Orders</a>
+        <a href="Login.php">Logout</a>
+    </div>
+</div>
+</body>
