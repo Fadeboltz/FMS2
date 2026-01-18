788c9bb32f67850eb2b92a56917f781aa5b2a433
Nabil Nasim <nabilmohammednasim@gmail.com>
2026-01-18 19:20:14 +0600

    login protection and oder id validation done

--- Customer/Controller Logic/OrderDetailsController.php
+++ Customer/Controller Logic/OrderDetailsController.php
@@ -2,5 +2,17 @@
 session_start();
 include '../Model/db.php';
 
+//login protection
+if (!isset($_SESSION['user_id'])) {
+    header("Location: Login.php");
+    exit();
+}
+
+//validate order_id
+if (!isset($_GET['order_id'])) {
+    header("Location: Order.php");
+    exit();
+}
+
 
 ?>
