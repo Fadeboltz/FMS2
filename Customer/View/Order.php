fa1c95c69a09ca895c2e04f485696d00d4041b83
Nabil Nasim <nabilmohammednasim@gmail.com>
2026-01-18 13:48:12 +0600

    order history done

--- Customer/View/Order.php
+++ Customer/View/Order.php
@@ -32,3 +32,31 @@ if (mysqli_num_rows($ongoing_orders) > 0) {
     echo "<p>No ongoing orders</p>";
 }
 ?>
+
+<hr>
+
+<h2>Order History</h2>
+
+<?php
+if (mysqli_num_rows($order_history) > 0) {
+    while ($order = mysqli_fetch_assoc($order_history)) {
+?>
+        <div class="order-card">
+            <strong>Order #<?php echo $order['order_id']; ?></strong><br>
+            Total: Tk <?php echo $order['total_amount']; ?><br>
+            Status: <?php echo $order['order_status']; ?><br>
+            Date: <?php echo $order['order_time']; ?><br>
+
+            <a href="OrderDetails.php?order_id=<?php echo $order['order_id']; ?>">
+                View Details
+            </a>
+        </div>
+<?php
+    }
+} else {
+    echo "<p>No past orders</p>";
+}
+?>
+
+</body>
+</html>
