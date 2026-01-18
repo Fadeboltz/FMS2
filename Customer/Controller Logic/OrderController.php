fe5f65257cd36737dc11933943b99b9169460c81
Nabil Nasim <nabilmohammednasim@gmail.com>
2026-01-18 14:21:26 +0600

    create order, insert order and clear cart validdation + query written

--- Customer/Controller Logic/OrderController.php
+++ Customer/Controller Logic/OrderController.php
@@ -21,4 +21,38 @@ if (isset($_POST['checkout'])) {
     foreach ($_SESSION['cart'] as $item) {
         $total += $item['price'] * $item['qty'];
     }
+
+    //Create order 
+    mysqli_query(
+        $conn,
+        "INSERT INTO orders (user_id, total_amount, order_status)
+         VALUES ($user_id, $total, 'pending')"
+    );
+
+    $order_id = mysqli_insert_id($conn);
+
+    // Insert order items 
+    foreach ($_SESSION['cart'] as $item) {
+        $menu_id = $item['menu_id'];
+        $price   = $item['price'];
+        $qty     = $item['qty'];
+
+        mysqli_query(
+            $conn,
+            "INSERT INTO order_items (order_id, menu_id, price, quantity)
+             VALUES ($order_id, $menu_id, $price, $qty)"
+        );
+    }
+
+    // Clear cart 
+    $_SESSION['cart'] = [];
+
+    $_SESSION['order_success'] = "Order placed successfully!";
+
+    header("Location: ../View/Order.php");
+    exit();
+}
+
+
+
 ?>
