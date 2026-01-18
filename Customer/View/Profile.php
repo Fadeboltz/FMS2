84b34c94a2af466b5a12bb77af88a8e619af0463
Nabil Nasim <nabilmohammednasim@gmail.com>
2026-01-18 22:26:31 +0600

    profile html written

---
+++ Customer/View/Profile.php
@@ -0,0 +1,9 @@
+<?php include '../Controller Logic/ProfileController.php'; ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My Profile</title>
+    <link rel="stylesheet" href="../Stylesheet/Profile.css">
+</head>
+<body>
